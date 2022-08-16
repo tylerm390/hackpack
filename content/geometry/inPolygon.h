8a069b6a1b9555279e68923d76a1be2ad6adab37
tylerm390 <77948992+tylerm390@users.noreply.github.com>
2022-08-15 21:14:50 -0400

    Added Polygon Area and Point in Polygon

---
+++ content/geometry/inPolygon.h
@@ -0,0 +1,17 @@
+/**
+ * Description: Uses the cutting-ray test to see if a point is inside a polygon.
+ * Usage: Returns 0 if outside, 1 if strictly inside, and 2 if on.
+ * Status: tested on kattis::pointinpolygon
+ */
+
+template<class P> int inPoly(vector<P> poly, P p) {
+    bool good = false; int n = sz(poly);
+    auto crosses = [](P s, P e, P p) { 
+        return ((e.y >= p.y) - (s.y >= p.y)) * p.cross(s, e) > 0; 
+    };
+    for(int i = 0; i < n; i++){
+        if(onSeg(poly[i], poly[(i+1)%n], p)) return 2;
+        good ^= crosses(poly[i], poly[(i+1)%n], p);
+    }
+    return good;
+}
---
+++ content/geometry/polygonArea.h
@@ -0,0 +1,14 @@
+/**
+ * Description: Uses shoelace theorem to find the area of a polygon. 
+ * Usage: If area is negative, points are given cw, otherwise points are given ccw.
+ * Status: Tested on kattis::polygonarea
+ */
+
+#pragma once
+
+template<class T> ld polyArea(vector<pnt<T>> poly) {
+    int n = sz(poly); T area = 0;
+    for(int i = 0; i < n; i++)
+        area += poly[i].cross(poly[(i+1)%n]);
+    return area / 2.0L;
+}
